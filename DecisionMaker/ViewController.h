7abf91c7eba670ff30004b622b38ac7c3613f95a
Calvin Chestnut <cchestnut@Calvins-MacBook-Pro.local>
2014-07-16 10:26:12 -0400

    Initial Commit

---
+++ DecisionMaker/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DecisionMaker
+//
+//  Created by Calvin Chestnut on 7/16/14.
+//  Copyright (c) 2014 Calvin Chestnut. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ DecisionMaker/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  DecisionMaker
+//
+//  Created by Calvin Chestnut on 7/16/14.
+//  Copyright (c) 2014 Calvin Chestnut. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+            
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
